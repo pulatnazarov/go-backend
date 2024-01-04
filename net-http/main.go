f842f9e77ec3807284ff617ff698b0237b508c3e
Pulat Nazarov <pulatnazarov8899@gmail.com>
2024-01-04 19:06:21 +0500

    net/http

---
+++ net-http/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+func generateRandom() int {
+	return rand.Intn(100)
+}
+
+func generate(w http.ResponseWriter, r *http.Request) {
+	randomNumber := generateRandom()
+
+	str := "random number is: " + strconv.Itoa(randomNumber)
+
+	if _, err := w.Write([]byte(str)); err != nil {
+		fmt.Println("Err")
+		return
+	}
+
+	fmt.Println("done!")
+}
+
+func main() {
+	// POST => body => save db => response => return successefully saved || id (uuid)
+	// GET => no body => id => response => return data
+	// PUT => body => update => response => return data || id
+	// DELETE => no body => id => response => return successefully deleted
+
+	// http.HandleFunc("/generate", generate)
+
+	// fmt.Println("program is running ...")
+	// http.ListenAndServe("localhost:8080", nil)
+
+	var name1, name2, name3 string
+
+	fmt.Print("enter name: ")
+	fmt.Scan(&name1, &name2, &name3)
+
+	client := http.Client{}
+
+	// url := fmt.Sprintf("https://api.agify.io?name[]=%s&name[]=%s&name[]=%s", name1, name2, name3)
+	url := "https://api.agify.io"
+
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Println("error while creating request", err.Error())
+		return
+	}
+
+	query := request.URL.Query()
+	query.Add("name[]", name1)
+	query.Add("name[]", name2)
+	query.Add("name[]", name3)
+	request.URL.RawQuery = query.Encode()
+
+	fmt.Println("request url", request.URL.String())
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println("error while sending request", err.Error())
+		return
+	}
+
+	p := []Person{}
+
+	b, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("error while reading body", err.Error())
+		return
+	}
+
+	if err := json.Unmarshal(b, &p); err != nil {
+		fmt.Println("error while unmarshalling data", err.Error())
+		return
+	}
+
+	for _, v := range p {
+		fmt.Println(v.Age, v.Name, v.Count)
+	}
+}
+
+type Person struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Count int    `json:"count"`
+}
